bce1454172a9fe27343647974958218ff089f8f5
Jeung hak Ham <thisway8788@gmail.com>
2022-07-18 01:15:58 -0400

    Meme content added also css added

--- components/Meme.js
+++ components/Meme.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Meme() {
   return (
     <main>
-      <form>
-        <input type="text" />
-        <input type="text" />
-        <button>Get a new meme image!!</button>
+      <form className="form">
+        <input className="form--input" type="text" placeholder="Top text" />
+        <input className="form--input" type="text" placeholder="Bottom text" />
+        <button className="form--button">Get a new meme image💩</button>
       </form>
     </main>
   );
