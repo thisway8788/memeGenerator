b281ef29a6634edd15e1d6bc5095704503e0fbb2
Jeung hak Ham <thisway8788@gmail.com>
2022-07-18 01:03:25 -0400

    Meme jsx added with input tags and button

---
+++ components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meme() {
+  return (
+    <main>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <button>Get a new meme image!!</button>
+      </form>
+    </main>
+  );
+}
