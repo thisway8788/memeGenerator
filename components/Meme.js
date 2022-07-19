b6c0bf3aac00955930e553058577a8674ac027d6
Jeung hak Ham <thisway8788@gmail.com>
2022-07-19 15:50:01 -0400

    url changed

--- components/Meme.js
+++ components/Meme.js
@@ -1,13 +1,24 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+  let url;
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+  }
+
   return (
     <main>
-      <form className="form">
+      <p>{url}</p>
+      <div className="form">
         <input className="form--input" type="text" placeholder="Top text" />
         <input className="form--input" type="text" placeholder="Bottom text" />
-        <button className="form--button">Get a new meme image💩</button>
-      </form>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image💩
+        </button>
+      </div>
     </main>
   );
 }
